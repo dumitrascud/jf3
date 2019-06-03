bb41153841e4d2b83b45e2b634f89e598c932881
Dinu Dumitrascu <dumitrascu.dinu@gmail.com>
2019-06-03 15:22:40 +0300

    Removed unsuccessfull added files

--- HrManager.java
+++
@@ -1,7 +0,0 @@
-public class HrManager
-{
-	public static void main (String[] args)
-	{
-		System.out.println ("Hi, I'm HR Manager");
-	}
-}
--- Programmer.java
+++
@@ -1,7 +0,0 @@
-public class Programmer
-{
-	public static void main (String[] args)
-	{
-		System.out.println ("Hello everybody, I'm the programmer");
-	}
-}
