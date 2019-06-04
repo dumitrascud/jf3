353f1732fc6490cea6f4506d325543b63897f7d7
Dinu Dumitrascu <dumitrascu.dinu@gmail.com>
2019-06-04 13:16:02 +0300

    2-nd commitment, added multiple variable declaration.

--- Lesson 4/LearningVariables.java
+++ Lesson 4/LearningVariables.java
@@ -21,12 +21,17 @@ public class LearningVariables
 		double k = 1.7e-038;
 		double l = 1.7e+38;
 		
-		boolean m = true;
+		boolean m = true; 
 		boolean n = false;
 		boolean y = true;
 		
 		char o = 65;
 		char p = 66;
+
+int q,r,s;
+String t,u,v;
+boolean da,x,nu;
+
 
 		String greeting = "Hello World"; //Here I got an error becaus i wrote "string" with lowercase "s".
 		String error   = "Here have to be an error message";
