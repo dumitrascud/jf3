3dd0536f88f5b46d4ad0a04cb94ee3c2809e886c
Dinu Dumitrascu <dumitrascu.dinu@gmail.com>
2019-06-04 12:35:13 +0300

    Lesson 4 homework: learning variables.

---
+++ Lesson 4/LearningVariables.java
@@ -0,0 +1,38 @@
+public class LearningVariables
+{
+	public static void main (String[] args)
+	{
+		byte a = -111;
+		byte b =  121;
+
+		short c = -20000;
+		short d =  30000;
+
+		int e = -5000000;
+		int f =  6000000;
+		int z =  110;
+
+		long g = -1000000000;
+		long h =  2000000000;
+		
+		float i = -5.21F;
+		float j =  6.12F;
+
+		double k = 1.7e-038;
+		double l = 1.7e+38;
+		
+		boolean m = true;
+		boolean n = false;
+		boolean y = true;
+		
+		char o = 65;
+		char p = 66;
+
+		String greeting = "Hello World"; //Here I got an error becaus i wrote "string" with lowercase "s".
+		String error   = "Here have to be an error message";
+		String confirmation = "Here have to be an successfull operation completeion message";
+
+		
+
+	}
+}
