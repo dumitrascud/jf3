a57fb43dbb509b6a812109ec77e371c40ee70d83
Dinu Dumitrascu <dumitrascu.dinu@gmail.com>
2019-06-04 13:04:11 +0300

    Lesson 5 homework: Working with variables.

---
+++ Lesson 5/WorkWithVariables.java
@@ -0,0 +1,73 @@
+public class WorkWithVariables
+{
+	public static void main (String[] args)
+	{
+
+	//Declaring boolean variables that will act as condidtions for identity registration requirements.
+		boolean pensioneer = false;
+		boolean male = true;
+		boolean female = true;
+		boolean divorced = false;
+		boolean hasnostudies = false;
+
+	//Declaring family status variables.
+		short familyMembers = 	1;
+		short systers = 0;
+		short brothers = 0;
+		short chidren = 1;
+		short parrents = 2;
+
+	//Declaring social information variables.
+		byte friends = 10;
+		byte exJobs = 5;
+		byte arests = 0;
+		byte surgeonInterventions = 0;
+		byte policeFines = 3;
+
+	//Declaring phisycal description variables.
+		int MinAge = 18;
+		int MaxAge = 62;
+		int CurrentAge = 32;
+		int height = 178;
+		int weight = 87;
+
+	//Declaring financial status variables.
+		long Askedsalary = 20000;
+		long minSalary = 1600;
+		long maxSalary = 15000;
+		long paydFines = 2;
+		long unpaydFines = 1;
+
+	//Declaring land status variables.
+		float lands = 1F;
+		float landLength = 22.3F;
+		float landWidth = 15.8F;
+		float landSurface = 352.34F;
+		float landLivingSurface = 152.18F;
+
+	//Declaring rooms surfaces (in square meters).
+		double kitchen = 12E4d;
+		double living = 15E4d;
+		double bedroom1 = 10E4d;
+		double bedroom2 = 11E4d;
+		double bathroom = 4e4d;
+
+
+	//Declaring adress variables.
+		char a = 1; //a=city, 1=capital.
+		char b = 4; //street which start With 4-th letter "D" - 'Dumitru-Rascanu.
+		char c = 31; // street Number.
+		char e = 1; //Enterance Number = 1.
+		char f = 15; //Flat number.
+
+	//declaring biographical variables.
+		String name = "John";
+		String surname = "Ionescu";
+		String patronymic = "Ion";
+		String motherName = "Maria";
+		String scoolGivenAlias = "jionica";
+
+	}
+
+}
+
