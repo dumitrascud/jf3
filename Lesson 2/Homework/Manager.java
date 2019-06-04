84f269a2f71f47d00077eb8e6ee7521ba370b715
Dinu Dumitrascu <dumitrascu.dinu@gmail.com>
2019-06-04 12:19:11 +0300

    Upload of Lesson 2 homework.

---
+++ Lesson 2/Homework/Manager.java
@@ -0,0 +1,7 @@
+public class Manager
+{
+	public static void main (String[] args)
+	{
+		System.out.println ("Hello, I'm your manager");
+	}
+}
---
+++ Lesson 2/HrManager.java
@@ -0,0 +1,7 @@
+public class HrManager
+{
+	public static void main (String[] args)
+	{
+		System.out.println ("Hi, I'm HR Manager");
+	}
+}
