8b661caa5b9cc0e13c2d080853ba75c7ed6be62f
Dinu Dumitrascu <Dinu.Dumitrascu@endava.com>
2019-06-07 16:05:03 +0300

    A fost adaugata aria.

--- Lesson 11/Perimetru.java
+++ Lesson 11/Perimetru.java
@@ -5,19 +5,20 @@ import java.util.Scanner;
 public class Perimetru {
     public static void main(String[] args) {
 
-        System.out.println("Introduceti valoarea lui X ");
+        System.out.println("Introduceti lungimea primei laturi  ");
         Scanner valoare = new Scanner(System.in);
         int x = valoare.nextInt();
 
-        System.out.println("Introduceti valoarea lui Y ");
+        System.out.println("Introduceti lungimea celei de a 2-a laturi ");
         int y = valoare.nextInt();
 
         int z = x + y;
+        int a = x * y;
 
         if (x == y){
-            System.out.println("Ati introdus lature de aceeasi lungime, caracteristic unui patrat ce are perimetrul = cu " + z);
+            System.out.println("Ati introdus lature de aceeasi lungime, caracteristic unui patrat ce are perimetrul = cu " + z +" si aria cu "+a);
          }else{
-             System.out.println("Ati introdus lature de lungime diferita, caracteristic unui dreptunghi ce are perimetrul = cu " + z);
+             System.out.println("Ati introdus lature de lungime diferita, caracteristic unui dreptunghi ce are perimetrul = cu " + z+" si aria cu "+a);
          }
         
     }
